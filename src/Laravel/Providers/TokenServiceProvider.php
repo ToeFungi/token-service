60ec789ac6c9cbd48b4fa13613806d6b98c9fd74
Alex Pickering <alexp@curve.group>
2018-07-27 09:12:06 +0200

    Set data
    Set audience and issuer

--- src/Laravel/Providers/TokenServiceProvider.php
+++ src/Laravel/Providers/TokenServiceProvider.php
@@ -15,10 +15,16 @@ class TokenServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(TokenService::class, function () {
-            $data = new ValidationData();
+            $token = new Token();
             $signer = new Sha256();
             $keychain = new Keychain();
-            $token = new Token();
+            $data = new ValidationData();
+
+            $tokenAud = getenv('TOKEN_AUD') ?: '';
+            $tokenIss = getenv('TOKEN_ISS') ?: '';
+
+            $data->setIssuer($tokenIss);
+            $data->setAudience($tokenAud);
 
             return new LcobucciToken($data, $signer, $keychain, $token);
         });
